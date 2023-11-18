c19381058d29acd0404b18350fc36e056b093ebf
Roman <romansp007@mail.ru>
2023-11-18 20:14:05 +0300

    Добавил метод

--- Program.cs
+++ Program.cs
@@ -4,15 +4,21 @@
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Write("Введите значение К1: ");
-double k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите знаяение К2: ");
-double k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение b1: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение b2: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-double x = (b2 - b1)/(k1 - k2);
-//double y = k1 * x + b1;
+double b2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine($"Значение х = {x}, значение y = {k1 * x + b1}");
+(double x, double y) = GetPoint(k1, k2, b1, b2);
+static (double x, double y) GetPoint(double k1, double k2, double b1, double b2){
+
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+Console.WriteLine($"Значение х = {x}, значение y = {y}");
 
